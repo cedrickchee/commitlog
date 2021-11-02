d155ed155e8172317ddbd0c2510e4ac8bc44b368
Cedric Chee <cedric@invictusbyte.com>
2021-11-03 01:33:11 +0800

    add more variables to client certs & Casbin config files

--- internal/config/files.go
+++ internal/config/files.go
@@ -6,14 +6,20 @@ import (
 )
 
 // config package contains the file paths to the certificate and private keys
-// for our CA and server as variables to make referencing them easier.
+// for our CA and server as variables to make referencing them easier. In
+// addition, it contains variables to the locations of the config files for
+// Casbin.
 
 var (
-	CAFile         = configFile("ca.pem")
-	ServerCertFile = configFile("server.pem")
-	ServerKeyFile  = configFile("server-key.pem")
-	ClientCertFile = configFile("client.pem")
-	ClientKeyFile  = configFile("client-key.pem")
+	CAFile               = configFile("ca.pem")
+	ServerCertFile       = configFile("server.pem")
+	ServerKeyFile        = configFile("server-key.pem")
+	RootClientCertFile   = configFile("root-client.pem")
+	RootClientKeyFile    = configFile("root-client-key.pem")
+	NobodyClientCertFile = configFile("nobody-client.pem")
+	NobodyClientKeyFile  = configFile("nobody-client-key.pem")
+	ACLModelFile         = configFile("model.conf")
+	ACLPolicyFile        = configFile("policy.csv")
 )
 
 func configFile(filename string) string {
