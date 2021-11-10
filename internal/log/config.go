de7aea5879db7bc3a8267bbe2faf99cabe9c284a
Cedric Chee <cedric@invictusbyte.com>
2021-11-10 21:53:51 +0800

    support configuring Raft in log's config

--- internal/log/config.go
+++ internal/log/config.go
@@ -1,7 +1,14 @@
 package log
 
+import "github.com/hashicorp/raft"
+
 // Config is the log's configuration
 type Config struct {
+	Raft struct {
+		raft.Config
+		StreamLayer *StreamLayer
+		Bootstrap   bool
+	}
 	Segment struct {
 		MaxStoreBytes uint64
 		MaxIndexBytes uint64
